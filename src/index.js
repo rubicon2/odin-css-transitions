2e3df8d560bd1c573cf3347ca9a93605d0690837
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-03-14 17:09:29 +0000

    Create animated square using transitions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './style.css';
+
+const { body } = document;
+
+const box = document.createElement('div');
+box.classList.add('box');
+body.appendChild(box);
+
+function rangedRandomInt(min, max) {
+  return Math.round(min + (max - min) * Math.random());
+}
+
+function randomRgb() {
+  return `rgb(${rangedRandomInt(0, 255)}, ${rangedRandomInt(0, 255)}, ${rangedRandomInt(0, 255)})`;
+}
+
+setInterval(() => {
+  box.style.backgroundColor = randomRgb();
+}, 5000);
